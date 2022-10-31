b2736b88f1e0bd3d6d0131580acaef24207fc3d5
alian120v <jonpatron1543@gmail.com>
2022-10-31 18:45:51 +0400

    0002

--- test/courier.py
+++ test/courier.py
@@ -1,3 +1,6 @@
+from turtle import distance
+
+
 spart=(0,0)
 class XY:
     def __init__(self,x,y):
@@ -14,7 +17,10 @@ class courier(XY):
         self.name_courier = name_courier
     def __str__(self):
         return f'x= {self.x}, y= {self.y}, имя курьера {self.name_courier}, его скорость {self.v}'
+    def time(self, other):
+        return (2*(self.distance(other)))/self.v
 p1=XY(2,2)
 cour1=courier(2,3,3,'Ivan')
 dis=p1.distance(cour1)
-print(ord)
+t=cour1.time(p1)
+print(dis)
